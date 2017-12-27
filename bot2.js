4772b523473f83d7b833336b773b44ca90962131
skyhrsevvv <34390367+skyhrsevvv@users.noreply.github.com>
2017-12-28 00:27:42 +0800

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -1,3 +1,9 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "AR15";
+
+
+
 client.on('message', msg => {
   if (msg.content === prefix +' 100') {
     msg.reply('\n 姓名:**888**\n 等級:**2星**\n 槍種:**000g  ');
