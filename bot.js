a38594599a31f4413621c596a73994d01f7d534a
skyhrsevvv <34390367+skyhrsevvv@users.noreply.github.com>
2017-12-27 23:50:52 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const economy = require('discord-eco');
+const prefix = "AR15";
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+  console.log('Bot started.');
+  client.on('ready', msg => {
+  console.log('Bot operational.');
+  client.user.setGame('炸彈人');
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix +' help') {
+    msg.reply('你好 我是最可靠的AR15 只要AR15 人型/裝備時間 的話你就可以查到你想要的東西喔!');
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
