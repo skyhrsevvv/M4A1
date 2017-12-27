c1299e5f34cf88a42133e3503dae6405bda2fee1
skyhrsevvv <34390367+skyhrsevvv@users.noreply.github.com>
2017-12-28 00:10:54 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,12 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+  console.log('Bot started.');
+  client.on('ready', msg => {
+  console.log('Bot operational.');
+  client.user.setGame('炸彈人');
+});
+
 client.on('message', msg => {
   if (msg.content === prefix +' help') {
     msg.reply('你好 我是最可靠的AR15 只要AR15 人型/裝備時間 的話你就可以查到你想要的東西喔!');
