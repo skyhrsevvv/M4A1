5d327848c2cdba176ee48b64cfcdabf262578b86
skyhrsevvv <34390367+skyhrsevvv@users.noreply.github.com>
2017-12-28 00:18:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "AR15";
+const prefix = "G41";
 
 client.on('ready', () => {
     console.log('I am ready!');
