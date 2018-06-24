13b4392a32b2cbf9af6a5d680f6737468907a686
skyhrsevvv <34390367+skyhrsevvv@users.noreply.github.com>
2018-06-24 22:22:53 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ const prefix = "AR15";
  console.log('Bot started.');
   client.on('ready', msg => {
   console.log('Bot operational.');
-  client.user.setGame('炸彈人');
+  client.setGame('炸彈人');
 });
 
-client.setGame("Some game title here");
+
 
 
 client.on('message', msg => {
