1cc61a1f4a81753b2bdb535e1fee8454e403e68c
skyhrsevvv <34390367+skyhrsevvv@users.noreply.github.com>
2018-07-12 16:19:29 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,6 @@ client.on('ready',() => {
 
  console.log('Bot started.');
   client.on('ready', msg => {
-  console.log('Bot operational.');
   client.user.setStatus("Online")
   client.user.setGame('炸彈人');
 });
