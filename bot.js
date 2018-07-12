9a832a71e96cf61aa00e5b3c9a441a2402d65355
skyhrsevvv <34390367+skyhrsevvv@users.noreply.github.com>
2018-07-12 16:15:19 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "AR15";
 
-bot.on('ready',() => {
+client.on('ready',() => {
   console.log('Bot Launched...')
 
   bot.user.setStatus("Online")
